c9d702fcfdcb7f804fb7dc8af397c52945f73fcb
Deepesh Rathore <deepeshrathore72@gmail.com>
2023-12-09 23:05:37 +0530

    Add new files

---
+++ app.js
@@ -0,0 +1,7 @@
+// Basic commands:-1.clone, 2.status, 3.add, 4.commit, 5.push
+// Clone :- cloning a repository on our local machine. git clone <-some link->
+// status :- displays the state of the code. git status
+// File status life cycle :- 1.untracked, 2.unmodified, 3.modified, 4.staged
+// add :- adds new or changed files in your working directory to the git staging area.git add <-file name->
+// commit :- it is the record of change. git commit -m "some message".
+// Push:-upload local repo content to remote repo. "git push origin main"
